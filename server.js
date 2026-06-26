22d8ec4fa0fec7ebc6a1ab2e885dfede93fa4156
Romar Vincent <romarmacay04@gmail.com>
2026-06-26 11:35:31 +0800

    Added express and core dependencies

--- server.js
+++ server.js
@@ -344,6 +344,7 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 server.listen(PORT, () => {
-    console.log(`MatchYap Server running on port ${PORT}`);
+  console.log(`MatchYap Server running on port ${PORT}`);
 });
